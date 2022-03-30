6de3cc4f87a4f807c7051dee85ba059613b96191
Jacob D. Parr <me@jacobparr.com>
2022-03-30 09:43:13 -0500

    x

--- dbacademy/gems/setup.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-import setuptools
-from setuptools import find_packages
-
-setuptools.setup(
-    name="dbacademy-gems",
-    version="0.1",
-    packages=find_packages(),
-)
