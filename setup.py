f5ea2b3e1a29e2554d0d0827b3fd3a8472cbe29b
Jacob D. Parr <me@jacobparr.com>
2022-04-01 11:59:29 -0500

    x

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from setuptools import find_packages
 
 setuptools.setup(
-    name="dbacademy-gems",
+    name="dbacademy-rest",
     version="0.1",
     packages=find_packages(),
 )
