5d0a82db875096323e99cc6e55e01a814e054857
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-05 19:53:13 -0500

    More refactoring

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 import setuptools
 from setuptools import find_packages
 
+
+def find_dbacademy_packages():
+    packages = find_packages(where="src")
+    if "dbacademy" in packages:
+        del packages[packages.index("dbacademy")]
+    print("-"*80)
+    print(packages)
+    print("-"*80)
+    return packages
+
+
 setuptools.setup(
     name="dbacademy-gems",
     version="0.1",
     package_dir={"dbacademy": "src/dbacademy"},
-    packages=find_packages("src")
+    packages=find_dbacademy_packages()
 )
