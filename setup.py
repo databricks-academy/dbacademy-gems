3423eec6e1feb3bd9b80899e990f00435d959c59
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-05 19:13:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import setuptools
+from setuptools import find_packages
 
 setuptools.setup(
     name="dbacademy-gems",
     version="0.1",
-    package_dir={"dbacademy": "src"},
-    packages=["dbacademy.dbgems"],
+    package_dir={"dbacademy": "src/dbacademy"},
+    packages=find_packages("src")
 )
