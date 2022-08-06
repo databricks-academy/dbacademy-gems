767bf06d46e5317136bddc6c1080c0fbd917d274
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-05 19:10:20 -0500

    Revert "Update setup.py"
    This reverts commit a6188713263df4426d304b067c5c9b9e56dd601d.

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import setuptools
-from setuptools import find_packages
 
 setuptools.setup(
     name="dbacademy-gems",
     version="0.1",
-    package_dir={"dbacademy": "src/dbacademy"},
-    packages=find_packages("src")
+    package_dir={"dbacademy": "src"},
+    packages=["dbacademy.dbgems"],
 )
